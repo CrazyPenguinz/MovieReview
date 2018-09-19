a094ff6cb953f39a054045b8d50a2a186e0a753e
mote <natthapon.cha@ku.th>
2018-09-19 07:48:16 +0700

    create home ui controller

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class HomeController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 
+public class HomeController {
+    @FXML private MenuBar menuBar = new MenuBar();
+    @FXML private Menu menu = new Menu();
+    @FXML private MenuItem action = new MenuItem("Action");
+
+    public void initialize() {
+        menu.setGraphic(new ImageView("/MenuIcon.png"));
+        menu.getItems().add(action);
+        menuBar.getMenus().add(menu);
+    }
 }
